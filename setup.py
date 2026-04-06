5f863805a3f5681218708909f9ccb47bd0226523
GeiserX <9169332+GeiserX@users.noreply.github.com>
2026-04-07 00:45:08 +0200

    Fix PyPI publish: add explicit license field, remove redundant rebuild
    Newer setuptools auto-generates license-file metadata from LICENSE that
    twine rejects. Adding explicit license= to setup.py fixes this. Also
    removed the redundant package rebuild in the release job since it
    already downloads pre-built artifacts from the build-package job.

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/GeiserX/Wayback-Diff",
+    license="GPL-3.0-only",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
